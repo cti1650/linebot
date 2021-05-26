c2e4aed246a0bbfbfe0bb142f33854196f2c7819
cti1650 <cti1650@gmail.com>
2021-05-27 06:46:51 +0900

    テンプレート書き換え

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-module.exports = async function App(context) {
-  await context.sendText('Welcome to Bottender');
+const { router, text } = require('bottender/router');
+
+async function SayHi(context) {
+  await context.sendText('Hi!');
+}
+
+async function Unknown(context) {
+  await context.sendText('Sorry, I don’t know what you say.');
+}
+
+module.export = function App(context) {
+  return router([text('hi', SayHi), text('*', Unknown)]);
 };
