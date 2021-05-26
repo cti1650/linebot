9a2d5f628b258dc9760f781550b7a4f4d31295e2
cti1650 <cti1650@gmail.com>
2021-05-27 07:03:37 +0900

    書き換え

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
-const { router, text } = require('bottender/router');
+import { Action, LineContext, LineEvent } from 'bottender';
+import { router, line } from 'bottender/router';
+import { AnyContext } from 'bottender/dist/types';
 
-async function SayHi(context) {
-  await context.sendText('Hi!');
+const messageContext = line.message(async (context) => {
+  const event = context.event;
+
+  switch (event.message?.type) {
+    case 'text':
+      await context.replyText(event.message.text);
+      break;
+    case 'image':
+      const imageBuffer = await context.getMessageContent();
+      await context.replyText('画像を受け取りました');
+      break;
+    default:
+      await context.replyText('テキストを送ってください。');
+      break;
+  }
+});
+
+const FollowContext = line.follow(async (context) => {
+  await context.replyText('フォローありがとうございます。');
+});
+
+const AnyContext = line.any(async (context) => {
+  await context.replyText('おっけー');
+});
+
+export default async function App() {
+  return router([messageContext, FollowContext, AnyContext]);
 }
-
-async function Unknown(context) {
-  await context.sendText('Sorry, I don’t know what you say.');
-}
-
-module.export = function App(context) {
-  return router([text('hi', SayHi), text('*', Unknown)]);
-};
